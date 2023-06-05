97763a5451fa16a30a399fdbf9475c12ebef9e63
pond-e <haruto2266@icloud.com>
2023-06-05 22:01:57 +0900

    support width long img

--- main.py
+++ main.py
@@ -20,25 +20,40 @@ def most_used_color(img):
 
 if __name__ == '__main__':
     args = sys.argv
+    if args[1] == '-i':
+        output_color_r = args[2]
+        output_color_g = args[3]
+        output_color_b = args[4]
+        input_file_name = args[5]
+    else:
+        input_file_name = args[1]
     
-    background_img = cv2.imread(args[1])
+    background_img = cv2.imread(input_file_name)
     background_img_height = background_img.shape[0]
     background_img_width = background_img.shape[1]
-    output_width = int(background_img_height * (16/9))
-    output_height = background_img_height
+    if background_img_height > background_img_width:
+        output_width = int(background_img_height * (16/9))
+        output_height = background_img_height
+    else:
+        output_width = background_img_width
+        output_height = int(background_img_width * (9/16))
 
-    (b, g, r) = most_used_color(background_img)
+    if args[1] != '-i':
+        (output_color_b, output_color_g, output_color_r) = most_used_color(background_img)
 
-    padding_b = np.full((output_height, output_width), b)
-    padding_g = np.full((output_height, output_width), g)
-    padding_r = np.full((output_height, output_width), r)
+    padding_b = np.full((output_height, output_width), output_color_b)
+    padding_g = np.full((output_height, output_width), output_color_g)
+    padding_r = np.full((output_height, output_width), output_color_r)
     padding = np.stack([padding_b, padding_g, padding_r], 2)
 
     width_adjust = (output_width//2 + background_img_width//2) - (output_width//2 - background_img_width//2) - background_img_width
     # image synthesis
     padding[0:background_img_height, (output_width//2 - background_img_width//2):(output_width//2 + background_img_width//2 - width_adjust)] = background_img
     
-    length_to_file_extension = len(args[1])
-    while(args[1][length_to_file_extension - 1] != '.'):
+    
+
+    length_to_file_extension = len(input_file_name)
+    while(input_file_name[length_to_file_extension - 1] != '.'):
         length_to_file_extension -= 1
-    cv2.imwrite(args[1][:length_to_file_extension - 1]+'_desktop.png', padding)
+    cv2.imwrite(input_file_name[:length_to_file_extension - 1]+'_desktop.png', padding)
+    print("finish!")
